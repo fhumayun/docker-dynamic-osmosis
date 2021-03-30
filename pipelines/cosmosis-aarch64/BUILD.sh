c9afa617ea1f01c4cc46df1a525748f7b8de2734
Andrea Tupini <tupini07@gmail.com>
2021-03-30 09:34:48 -0600

    add container definition for cosmosis ARM build pipeline

---
+++ pipelines/cosmosis-aarch64/BUILD.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+#? This docker image will be compiled to ARM64-v8 using Docker's cross compilation functionality. 
+#? The resulting container should work fine on Raspberry Pi devices, but it's main goal is that of
+#? running on a AWS CodeBuild ARM machine.
+
+scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+IMAGE_ID=wti-build
+IMAGE_NAME=cosmosis-aarch64
+
+IMAGE_VERSION=1.0.0
+
+REGISTRY_URL="247011381634.dkr.ecr.us-west-2.amazonaws.com"
+
+IMAGE_TAG=$IMAGE_ID/$IMAGE_NAME:$IMAGE_VERSION
+
+# Create a new multiarch builder by executing 
+docker buildx create --name comsosis-aarch64-builder
+
+# Bootstrap the new builder 
+docker buildx use comsosis-aarch64-builder 
+docker buildx inspect --bootstrap
+
+# Build image, tag, and push to ECR
+docker buildx build \
+    --memory=8000mb \
+    --platform=linux/arm64/v8 \
+    --tag "$REGISTRY_URL/$IMAGE_TAG" $scriptDir \
+    --push
--- pipelines/cosmosis-sonarqube/BUILD.sh
+++ pipelines/cosmosis-sonarqube/BUILD.sh
@@ -6,11 +6,12 @@ IMAGE_ID=wti-build
 IMAGE_NAME=cosmosis-sonarqube
 IMAGE_VERSION=1.0.0
 
-IMAGE_TAG=$IMAGE_ID/$IMAGE_NAME:$IMAGE_VERSION
+REGISTRY_URL="247011381634.dkr.ecr.us-west-2.amazonaws.com"
+
+IMAGE_TAG="$REGISTRY_URL/$IMAGE_ID/$IMAGE_NAME:$IMAGE_VERSION"
 
 # Build image
 docker build --tag $IMAGE_TAG $scriptDir
 
 # Tag and push to ECR
-docker tag $IMAGE_TAG 247011381634.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_TAG
-docker push 247011381634.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_TAG
+docker push $IMAGE_TAG
