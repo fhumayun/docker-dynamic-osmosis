d9ffc3f547446f527a7d7ccd033711e1d20abc79
Andrea Tupini <tupini07@gmail.com>
2021-04-14 11:43:40 -0600

    Add example dockerfile of how we can run osmosis in a container

--- osmosis/docker-entrypoint.sh
+++ osmosis/docker-entrypoint.sh
@@ -1,6 +1,24 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 cd /app/cloud 
-nohup ./bin/cloud -w ../manager/ -s ../star_local.jks &> /dev/null &
 
-watch -n 1 cat log/status.txt
+NOHUP_PID="-0"
+
+function start_cloud () {
+    nohup ./bin/cloud -w ../manager/ -s ../star_local.jks &> /dev/null &
+    rm /app/cloud/nohup.out
+    NOHUP_PID="$!"
+}
+
+while true; do
+    clear
+    cat log/status.txt
+
+    # check if cloud process is active and restart otherwise
+    if [ -z "$(ps aux | grep $NOHUP_PID | grep -v grep)" ]; then
+        start_cloud
+        sleep 5
+    fi
+
+    sleep 2
+done
--- osmosis/launch_cloud.sh
+++ osmosis/launch_cloud.sh
@@ -1,11 +1,38 @@
 #!/usr/bin/env bash
 
-I_NAME=osmosis-java-runner
+set -e 
 
+if [ ! -e "osmosis.tar.gz" ]; then
+    mkdir osmosis
+    cd osmosis
+
+    # get star_local certificate
+    CERT_PATH=$(ls -l ~/osmosis/star_local.jks | cut -d">" -f 2-)
+    cp $CERT_PATH ./star_local.jks
+
+    # bundle cloud
+    aws s3 cp s3://osmosis-cicd-artifactbucket-1s4qfttb1z9mh/build/cloud-dev-0.12.5-20210319T184924Z.tar.gz ./cloud.tar.gz
+    tar xf cloud.tar.gz
+    rm cloud.tar.gz
+
+    # bundle manager
+    mkdir manager
+    cd manager
+    aws s3 cp s3://osmosis-cicd-artifactbucket-1s4qfttb1z9mh/manager/manager-dev-0.12.10-20210408T202152UTC.tar.gz ./manager.tar.gz
+    tar xf manager.tar.gz
+    rm manager.tar.gz
+
+    # compress everything
+    cd ..
+    tar -czf ../osmosis.tar.gz *
+    cd ..
+    rm -rf osmosis
+fi
+
+I_NAME=osmosis-java-runner
 docker build --tag $I_NAME .
 
 docker run \
     -it --rm \
     -p 8522:8522 \
-    -v $(pwd)/executables:/app \
     $I_NAME
