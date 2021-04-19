40e107df22a2f9d1b03c19f90a7012c2c6777978
Ricardo <rchoy@cecropiasolutions.com>
2021-04-19 13:14:41 -0500

    fixing bug on docker-entrypoint.sh

---
+++ osmosis/README.md
@@ -0,0 +1,8 @@
+# Pipelines Containers
+
+## cosmosis-sonarqube
+
+[ECR Page](https://us-west-2.console.aws.amazon.com/ecr/repositories/private/247011381634/wti-build/cosmosis-sonarqube?region=us-west-2)
+
+This is the container used to perform and upload the SonarQube analysis for the Cosmosis project. It prepackages all of
+the necessary dependencies to get the analysis to work correctly.
--- osmosis/docker-entrypoint.sh
+++ osmosis/docker-entrypoint.sh
@@ -5,15 +5,14 @@ cd /app/cloud
 NOHUP_PID="-0"
 
 function start_cloud () {
-    # remove nohup   
+ 
     nohup ./bin/cloud -w ../manager/ -s ../star_local.jks &> /dev/null &
-    NOHUP_PID="$!"
+    NOHUP_PID="$!" 
 
 }
 
 start_cloud
-sleep 8
-echo $NOHUP_PID
+sleep 5
 kill -9 $NOHUP_PID &> /dev/null
 
 NOHUP_PID="-0"
@@ -28,28 +27,22 @@ if [ -n "$TCP" ]; then
       ARR="$ARR \"$x\""
     done
     ARR="$ARR ]"
-    #cat ./config/osmosis.json
-    sleep 2
-    echo $ARR
-    jq --version
-    jq ".tcp_discovery = $ARR" ./config/osmosis.json
-    sleep 2  
-    cat ./config/osmosis.json
-    #sed -i -Ee 's/\s*"tcp_discovery".*/"tcp_discovery": '"$ARR"',/g'  ./config/osmosis.json
+    jq ".tcp_discovery = $ARR" ./config/osmosis.json > ./config/osmosis2.json
+    mv -f ./config/osmosis2.json ./config/osmosis.json
 fi
 
-sed -i -Ee 's,\s*"id".*,"id": "'$CONTAINER_ID'"\,,g'  ./config/node.json
+jq '.id = "'$CONTAINER_ID'"' ./config/node.json  > ./config/node2.json 
+mv -f ./config/node2.json ./config/node.json
 
 # set hostname with env variable. with a check
 
 while true; do
-    #clear
-    #cat log/status.txt
+    clear
+    cat log/status.txt
 
-    # check if cloud process is active and restart otherwise
-    if [ -z "$(ps aux | grep $NOHUP_PID | grep -v grep)" ]; then
-        start_cloud
-        sleep 5
+    if ! ps -p $NOHUP_PID  > /dev/null; then
+      start_cloud
+      sleep 5
     fi
 
     sleep 2
