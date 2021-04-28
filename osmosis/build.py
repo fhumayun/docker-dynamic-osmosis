a19a8aa678c53b93be84d7d44ebb2fb3a8e1eb72
ricardo <rchoy@cecropiasolutions.com>
2021-04-28 12:25:01 -0500

    Script working with different versions of osmosis and pushing the images to AWS

--- osmosis/build.py
+++ osmosis/build.py
@@ -4,18 +4,41 @@ import os
 import argparse
 import lib
 
-parser = argparse.ArgumentParser(description="""Build docker images for running osmosis 
-                                "or odevapp on Ubuntu 20.01 or Centos8 and publish them 
-                                "to AWS ECR or run test containers. It is required to have 
-                                "the star_local.jks file located on docker/osmosis folder
-                                "to provide it to the containers.""")
+parser = argparse.ArgumentParser(description="""Build docker images for running 
+                                odevapp on Ubuntu 20.01 or Centos8 and publish them 
+                                to AWS ECR or run test containers.""")
 
-parser.add_argument("-o", "--odevapp", help="build the docker images for odevapp",
+parser.add_argument("-v", "--version", action="store", dest="version_number", help="odevapp version")
+parser.add_argument("-p", "--push", help="Push the images to ECR",
                     action="store_true")
-parser.add_argument("-v", "--version", action="store", dest="v", help="odevapp version")
+parser.add_argument("-r", "--run", help="Run sample containers",
+                    action="store_true")
 
+# Parse incoming parameters
 args = parser.parse_args()
 
-if args.odevapp:
-    lib.odevapp_files.getOdevappTar(args.v)
-    os.system("docker build --tag odevapp -f dockerfiles/Dockerfile.ubuntu.odevapp .")
+
+o_version = lib.odevapp_files.getOdevappTar(args.version_number)
+
+image_id = "wti-dev"
+image_name = "osmosis"
+image_version_ubuntu = "ubuntu20.10-x64"
+image_version_centos = "centos8-x64"
+registry_url="247011381634.dkr.ecr.us-west-2.amazonaws.com"
+
+image_tag_ubuntu = registry_url + "/" + image_id + "/" + image_name + ":osmosis" + image_version_ubuntu + o_version
+image_tag_centos = registry_url + "/" + image_id + "/" + image_name + ":osmosis" + image_version_centos + o_version
+
+# Build images
+os.system("docker build --tag %s -f %s/dockerfiles/Dockerfile.ubuntu.odevapp ." %(image_tag_ubuntu, os.getcwd()) )
+os.system("docker build --tag %s -f %s/dockerfiles/Dockerfile.centos.odevapp ." %(image_tag_centos, os.getcwd()) )
+
+if args.push:
+    os.system("docker push %s" %image_tag_ubuntu)
+    os.system("docker push %s" %image_tag_centos)
+
+if args.run:
+    os.system("docker run --name centosodevapp -h odevappcentos -d" \
+    " -e NODE_ID='11111111-aaaa-3333-4444-555555555555' %s" %image_tag_centos)
+    os.system("docker run --name ubuntuodevapp -h odevappubuntu -d" \
+    " -e NODE_ID='aaaaaaaa-1111-cccc-dddd-eeeeeeeeeeee' %s" %image_tag_ubuntu)
--- osmosis/lib/odevapp_files.py
+++ osmosis/lib/odevapp_files.py
@@ -4,18 +4,29 @@ import os
 import subprocess as sp
 import glob
 
+# Get the tar file from AWS S3
 def getOdevappTar(version):
-    # Get the tar file from AWS S3
+    
     if version is not None:
         version = "-" + version
     else:
         version = ""
+    
+    # Check the name of the artifact
+    odevapp_path = sp.getoutput("aws s3 ls s3://osmosis-cicd-artifactbucket-1s4qfttb1z9mh/build/ " \
+                                "| grep osmosis-dev%s | grep '\.gz$' | sort | tail -1" %version)
+    odevapp = sp.getoutput("echo %s | cut -d ' ' -f 4" %odevapp_path)
+    o_version = sp.getoutput("echo %s | cut -d '-' -f 3" %odevapp)
+
+    if len(sp.getoutput("echo %s | grep tar" %o_version)) > 0: 
+        o_version = sp.getoutput("echo %s | cut -d '.' -f -3" %o_version)
         
     if not os.path.isfile("odevapp%s.tar.gz" %version):
+        
+        # Remove previous files if exist
         if glob.glob("odevapp*.tar.gz"):
             os.system("rm odevapp*.tar.gz")
 
-        odevapp_path = sp.getoutput("aws s3 ls s3://osmosis-cicd-artifactbucket-1s4qfttb1z9mh/build/ " \
-                                    "| grep osmosis-dev%s | grep '\.gz$' | sort | tail -1" %version)
-        odevapp = sp.getoutput("echo %s | cut -d ' ' -f 4" %odevapp_path)
         os.system("aws s3 cp s3://osmosis-cicd-artifactbucket-1s4qfttb1z9mh/build/%s ./odevapp%s.tar.gz" % (odevapp, version))
+
+    return o_version
